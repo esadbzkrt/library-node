b96d939796c5c0c196ddba8e55fdd5adce5e434c
esadbzkrt <esadbzkrt@gmail.com>
2022-09-16 16:16:04 +0300

    Return Borrow Controller functions

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('../models/userModel');
+const Book = require('../models/bookModel');
 
 const getAllUsers = async (req, res) => {
     try {
@@ -27,8 +28,35 @@ const createUser = async (req, res) => {
     }
 }
 
+const borrowBook = async (req, res) => {
+    try {
+        const user = await User.findOne({userId: req.params.userId});
+        const book = await Book.findOne({bookId: req.params.bookId});
+        user.books.present.push(book);
+        await user.save();
+        res.status(201).json({message: 'borrowBook', user});
+    } catch (err) {
+        res.status(400).json({message: 'borrowBook', err});
+    }
+}
+
+const returnBook = async (req, res) => {
+    try {
+        const user = await User.findOne({userId: req.params.userId});
+        const book = await Book.findOne({bookId: req.params.bookId});
+        user.books.present.pull(book);
+        user.books.past.push(book);
+        await user.save();
+        res.status(201).json({message: 'returnBook', user});
+    } catch (err) {
+        res.status(400).json({message: 'returnBook', err});
+    }
+}
+
 module.exports = {
     getAllUsers,
     getUserById,
-    createUser
+    createUser,
+    borrowBook,
+    returnBook
 }
