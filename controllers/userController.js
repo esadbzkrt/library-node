b8115d651d7035ad9b68f2eedfbd9848687c882f
esadbzkrt <esadbzkrt@gmail.com>
2022-09-17 20:48:40 +0300

    Borrow Return Logic

--- controllers/userController.js
+++ controllers/userController.js
@@ -28,14 +28,14 @@ const getUserById = async (req, res) => {
             name: user.name,
             books: {
                 past: borrowHistory.filter(borrowHistory => borrowHistory.isReturned === true).map(borrowHistory => {
-                    return {
-                        name: borrowHistory.book.name,
-                        userScore: borrowHistory.userScore,
+                        return {
+                            name: borrowHistory.book.name,
+                            userScore: borrowHistory.userScore,
+                        }
                     }
-                }
                 ),
-                    },
-                present: user.presentBook ? user.presentBook: [],
+            },
+            present: user.presentBook ? user.presentBook : [],
         }
         res.status(200).json(userResponse);
     } catch (err) {
@@ -62,39 +62,58 @@ const borrowBook = async (req, res) => {
     try {
         const user = await User.findOne({userId: req.params.userId});
         const book = await Book.findOne({bookId: req.params.bookId});
-        const borrowHistory = new BorrowHistory({
-            user: user._id,
-            book: book._id,
-        });
-        await borrowHistory.save();
-        user.presentBook = book;
-        await user.save();
-        book.isAvailable = false;
-        await book.save();
-        res.status(201).json(borrowHistory);
-
+        if (!user.presentBook) {
+            if (book.isAvailable === true) {
+                const borrowHistory = new BorrowHistory({
+                    user: user._id,
+                    book: book._id,
+                });
+                await borrowHistory.save();
+                user.presentBook = book;
+                await user.save();
+                book.isAvailable = false;
+                await book.save();
+                res.status(201).json(borrowHistory);
+            } else {
+                res.status(400).json('Book is not available, please try another book');
+            }
+        } else {
+            res.status(400).json('User has a book, return it first');
+        }
     } catch (err) {
         res.status(400).json(err.message);
     }
-
-
 }
 
 const returnBook = async (req, res) => {
     try {
         const user = await User.findOne({userId: req.params.userId});
         const book = await Book.findOne({bookId: req.params.bookId});
-        const {userScore} = req.body;
-        const borrowHistory = await BorrowHistory.findOne({user: user._id, book: book._id});
-        borrowHistory.isReturned = true;
-        borrowHistory.userScore = userScore;
-        await borrowHistory.save();
-        user.presentBook = null;
-        await user.save();
-        book.isAvailable = true;
-        book.userScore.push(userScore);
-        await book.save();
-        res.status(201).json(book);
+
+        if (user.presentBook) {
+            if (book.isAvailable === false) {
+                const {userScore} = req.body;
+                const borrowHistory = await BorrowHistory.findOne({user: user._id, book: book._id});
+                if(borrowHistory){
+                    borrowHistory.isReturned = true;
+                    borrowHistory.userScore = userScore;
+                }else{
+                    res.status(400).json('This book is not borrowed by this user');
+                }
+                await borrowHistory.save();
+                user.presentBook = null;
+                await user.save();
+                book.isAvailable = true;
+                book.userScore.push(userScore);
+                await book.save();
+                res.status(201).json(book);
+            } else {
+                res.status(400).json('Book is not borrowed by this user');
+            }
+        } else {
+            res.status(400).json('User does not have a book, borrow a book first');
+        }
+
 
     } catch (err) {
         res.status(400).json(err.message);
